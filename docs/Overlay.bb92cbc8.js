a02030dfb798baf8412b814b341c3249f0cfe92b
Jonathan Beliën <dev@jbelien.be>
2020-05-26 17:22:17 +0200

    🎉 Initial commit

---
+++ docs/Overlay.bb92cbc8.js
@@ -0,0 +1,503 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+parcelRequire = (function (modules, cache, entry, globalName) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
+  var nodeRequire = typeof require === 'function' && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof parcelRequire === 'function' && parcelRequire;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+      localRequire.cache = {};
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports, this);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+  newRequire.register = function (id, exports) {
+    modules[id] = [function (require, module) {
+      module.exports = exports;
+    }, {}];
+  };
+
+  var error;
+  for (var i = 0; i < entry.length; i++) {
+    try {
+      newRequire(entry[i]);
+    } catch (e) {
+      // Save first error but execute all entries
+      if (!error) {
+        error = e;
+      }
+    }
+  }
+
+  if (entry.length) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(entry[entry.length - 1]);
+
+    // CommonJS
+    if (typeof exports === "object" && typeof module !== "undefined") {
+      module.exports = mainExports;
+
+    // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+     define(function () {
+       return mainExports;
+     });
+
+    // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+
+  // Override the current require with this new one
+  parcelRequire = newRequire;
+
+  if (error) {
+    // throw error from earlier, _after updating parcelRequire_
+    throw error;
+  }
+
+  return newRequire;
+})({"src/javascript/background.ts":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.destroy = exports.create = void 0;
+
+function create() {
+  document.body.style.overflow = "hidden";
+  var div = document.createElement("div");
+  div.className = "overlay";
+  div.addEventListener("click", function () {
+    destroy();
+    document.querySelector("body").style.overflow = "";
+  });
+  document.body.append(div);
+}
+
+exports.create = create;
+
+function destroy() {
+  var element = document.querySelector(".overlay");
+
+  if (element !== null) {
+    element.remove();
+  }
+}
+
+exports.destroy = destroy;
+},{}],"src/javascript/loading.ts":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+function default_1() {
+  var div = document.createElement("div");
+  div.className = "overlay-loading";
+  div.innerText = "Loading...";
+  document.querySelector(".overlay").append(div);
+}
+
+exports.default = default_1;
+},{}],"src/javascript/content.ts":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+function default_1() {
+  var div = document.createElement("div");
+  div.className = "overlay-content";
+  document.querySelector(".overlay").append(div);
+}
+
+exports.default = default_1;
+},{}],"src/javascript/image.ts":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var background_1 = require("./background");
+
+function determineImageOverlaySize(width, height, element) {
+  var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+  var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+
+  if (width > w || height > h) {
+    element.style.backgroundSize = "contain";
+  }
+}
+
+function default_1(image, width, height) {
+  var div = document.createElement("div");
+  div.className = "overlay-image";
+  div.style.backgroundImage = "url(" + image.src + ")";
+  div.addEventListener("click", function () {
+    background_1.destroy();
+    document.querySelector("body").style.overflow = "";
+  });
+  determineImageOverlaySize(width, height, div);
+  document.querySelector(".overlay-content").append(div);
+}
+
+exports.default = default_1;
+},{"./background":"src/javascript/background.ts"}],"src/javascript/caption.ts":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+function default_1(text, html) {
+  if (html === void 0) {
+    html = false;
+  }
+
+  var div = document.createElement("div");
+  div.className = "overlay-caption";
+
+  if (html === true) {
+    div.innerHTML = text;
+  } else {
+    div.innerText = text;
+  }
+
+  document.querySelector(".overlay-content").append(div);
+}
+
+exports.default = default_1;
+},{}],"src/javascript/Overlay.ts":[function(require,module,exports) {
+"use strict";
+
+var __importDefault = this && this.__importDefault || function (mod) {
+  return mod && mod.__esModule ? mod : {
+    "default": mod
+  };
+};
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var background_1 = require("./background");
+
+var loading_1 = __importDefault(require("./loading"));
+
+var content_1 = __importDefault(require("./content"));
+
+var image_1 = __importDefault(require("./image"));
+
+var caption_1 = __importDefault(require("./caption"));
+
+var Overlay =
+/** @class */
+function () {
+  function Overlay(element, captionCallback) {
+    this.element = element;
+    this.captionCallback = captionCallback;
+  }
+
+  Overlay.prototype.open = function () {
+    var _this = this;
+
+    var href = this.element.getAttribute("href");
+    document.querySelector("body").style.overflow = "hidden";
+    background_1.create();
+    loading_1.default();
+    content_1.default();
+    var image = new Image();
+    image.src = href;
+
+    image.onload = function () {
+      var width = image.naturalWidth;
+      var height = image.naturalHeight;
+      document.querySelector(".overlay-loading").remove();
+      image_1.default(image, width, height);
+
+      if (typeof _this.captionCallback !== "undefined") {
+        var caption = _this.captionCallback(_this.element);
+
+        caption_1.default(caption, true);
+      }
+    };
+  };
+
+  return Overlay;
+}();
+
+exports.default = Overlay; // See https://github.com/parcel-bundler/parcel/issues/766
+
+module.exports = Overlay;
+},{"./background":"src/javascript/background.ts","./loading":"src/javascript/loading.ts","./content":"src/javascript/content.ts","./image":"src/javascript/image.ts","./caption":"src/javascript/caption.ts"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+var global = arguments[3];
+var OVERLAY_ID = '__parcel__error__overlay__';
+var OldModule = module.bundle.Module;
+
+function Module(moduleName) {
+  OldModule.call(this, moduleName);
+  this.hot = {
+    data: module.bundle.hotData,
+    _acceptCallbacks: [],
+    _disposeCallbacks: [],
+    accept: function (fn) {
+      this._acceptCallbacks.push(fn || function () {});
+    },
+    dispose: function (fn) {
+      this._disposeCallbacks.push(fn);
+    }
+  };
+  module.bundle.hotData = null;
+}
+
+module.bundle.Module = Module;
+var checkedAssets, assetsToAccept;
+var parent = module.bundle.parent;
+
+if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
+  var hostname = "" || location.hostname;
+  var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54937" + '/');
+
+  ws.onmessage = function (event) {
+    checkedAssets = {};
+    assetsToAccept = [];
+    var data = JSON.parse(event.data);
+
+    if (data.type === 'update') {
+      var handled = false;
+      data.assets.forEach(function (asset) {
+        if (!asset.isNew) {
+          var didAccept = hmrAcceptCheck(global.parcelRequire, asset.id);
+
+          if (didAccept) {
+            handled = true;
+          }
+        }
+      }); // Enable HMR for CSS by default.
+
+      handled = handled || data.assets.every(function (asset) {
+        return asset.type === 'css' && asset.generated.js;
+      });
+
+      if (handled) {
+        console.clear();
+        data.assets.forEach(function (asset) {
+          hmrApply(global.parcelRequire, asset);
+        });
+        assetsToAccept.forEach(function (v) {
+          hmrAcceptRun(v[0], v[1]);
+        });
+      } else if (location.reload) {
+        // `location` global exists in a web worker context but lacks `.reload()` function.
+        location.reload();
+      }
+    }
+
+    if (data.type === 'reload') {
+      ws.close();
+
+      ws.onclose = function () {
+        location.reload();
+      };
+    }
+
+    if (data.type === 'error-resolved') {
+      console.log('[parcel] ✨ Error resolved');
+      removeErrorOverlay();
+    }
+
+    if (data.type === 'error') {
+      console.error('[parcel] 🚨  ' + data.error.message + '\n' + data.error.stack);
+      removeErrorOverlay();
+      var overlay = createErrorOverlay(data);
+      document.body.appendChild(overlay);
+    }
+  };
+}
+
+function removeErrorOverlay() {
+  var overlay = document.getElementById(OVERLAY_ID);
+
+  if (overlay) {
+    overlay.remove();
+  }
+}
+
+function createErrorOverlay(data) {
+  var overlay = document.createElement('div');
+  overlay.id = OVERLAY_ID; // html encode message and stack trace
+
+  var message = document.createElement('div');
+  var stackTrace = document.createElement('pre');
+  message.innerText = data.error.message;
+  stackTrace.innerText = data.error.stack;
+  overlay.innerHTML = '<div style="background: black; font-size: 16px; color: white; position: fixed; height: 100%; width: 100%; top: 0px; left: 0px; padding: 30px; opacity: 0.85; font-family: Menlo, Consolas, monospace; z-index: 9999;">' + '<span style="background: red; padding: 2px 4px; border-radius: 2px;">ERROR</span>' + '<span style="top: 2px; margin-left: 5px; position: relative;">🚨</span>' + '<div style="font-size: 18px; font-weight: bold; margin-top: 20px;">' + message.innerHTML + '</div>' + '<pre>' + stackTrace.innerHTML + '</pre>' + '</div>';
+  return overlay;
+}
+
+function getParents(bundle, id) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return [];
+  }
+
+  var parents = [];
+  var k, d, dep;
+
+  for (k in modules) {
+    for (d in modules[k][1]) {
+      dep = modules[k][1][d];
+
+      if (dep === id || Array.isArray(dep) && dep[dep.length - 1] === id) {
+        parents.push(k);
+      }
+    }
+  }
+
+  if (bundle.parent) {
+    parents = parents.concat(getParents(bundle.parent, id));
+  }
+
+  return parents;
+}
+
+function hmrApply(bundle, asset) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return;
+  }
+
+  if (modules[asset.id] || !bundle.parent) {
+    var fn = new Function('require', 'module', 'exports', asset.generated.js);
+    asset.isNew = !modules[asset.id];
+    modules[asset.id] = [fn, asset.deps];
+  } else if (bundle.parent) {
+    hmrApply(bundle.parent, asset);
+  }
+}
+
+function hmrAcceptCheck(bundle, id) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return;
+  }
+
+  if (!modules[id] && bundle.parent) {
+    return hmrAcceptCheck(bundle.parent, id);
+  }
+
+  if (checkedAssets[id]) {
+    return;
+  }
+
+  checkedAssets[id] = true;
+  var cached = bundle.cache[id];
+  assetsToAccept.push([bundle, id]);
+
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    return true;
+  }
+
+  return getParents(global.parcelRequire, id).some(function (id) {
+    return hmrAcceptCheck(global.parcelRequire, id);
+  });
+}
+
+function hmrAcceptRun(bundle, id) {
+  var cached = bundle.cache[id];
+  bundle.hotData = {};
+
+  if (cached) {
+    cached.hot.data = bundle.hotData;
+  }
+
+  if (cached && cached.hot && cached.hot._disposeCallbacks.length) {
+    cached.hot._disposeCallbacks.forEach(function (cb) {
+      cb(bundle.hotData);
+    });
+  }
+
+  delete bundle.cache[id];
+  bundle(id);
+  cached = bundle.cache[id];
+
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    cached.hot._acceptCallbacks.forEach(function (cb) {
+      cb();
+    });
+
+    return true;
+  }
+}
+},{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js","src/javascript/Overlay.ts"], "Overlay")
+//# sourceMappingURL=/Overlay.bb92cbc8.js.map
---
+++ src/javascript/Overlay.ts
@@ -0,0 +1,49 @@
+"use strict";
+
+import { create as createBackground } from "./background";
+import createLoading from "./loading";
+import createContent from "./content";
+import createImage from "./image";
+import createCaption from "./caption";
+
+class Overlay {
+  private element: HTMLElement;
+  private captionCallback: (element: HTMLElement) => string;
+
+  constructor(element: HTMLElement, captionCallback?: (element: HTMLElement) => string) {
+    this.element = element;
+    this.captionCallback = captionCallback;
+  }
+
+  public open():void {
+    const href = this.element.getAttribute("href");
+
+    document.querySelector("body").style.overflow = "hidden";
+
+    createBackground();
+    createLoading();
+    createContent();
+
+    const image = new Image();
+    image.src = href;
+    image.onload = () => {
+      const width = image.naturalWidth;
+      const height = image.naturalHeight;
+
+      document.querySelector(".overlay-loading").remove();
+
+      createImage(image, width, height);
+
+      if (typeof this.captionCallback !== "undefined") {
+        const caption = this.captionCallback(this.element);
+
+        createCaption(caption, true);
+      }
+    };
+  }
+}
+
+export { Overlay as default };
+
+// See https://github.com/parcel-bundler/parcel/issues/766
+module.exports = Overlay;
